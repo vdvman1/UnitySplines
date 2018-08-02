cc8c1e538365ccd2a83a0bad9801444499d6eed8
Stefan van der Velden <stefan.vdv@exemail.com.au>
2018-08-02 15:06:20 +1000

    Add very basic Line class

---
+++ UnitySplines/Assets/Scripts/Line.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace VDV.Spline
+{
+    public class Line : MonoBehaviour
+    {
+        [SerializeField]
+        protected Vector3[] points;
+    }
+
+}
